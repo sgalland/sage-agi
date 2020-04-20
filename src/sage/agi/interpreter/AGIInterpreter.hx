703985647071d1f124d36fc6b71c71f45cd17b3b
Sean Galland <s_galland@msn.com>
2020-04-20 16:41:46 -0600

    Made checkstyle fixes.

--- src/sage/agi/interpreter/AGIInterpreter.hx
+++ src/sage/agi/interpreter/AGIInterpreter.hx
@@ -18,25 +18,25 @@ class AGIInterpreter {
 		The various variables have differing meanings in how they are used. See specification for more information.
 		@see https://wiki.scummvm.org/index.php?title=AGI/Specifications/Internals#Variable
 	**/
-	public static var variables:Vector<AGIByte> = new Vector<AGIByte>(MAX_RESOURCES);
+	public static var VARIABLES:Vector<AGIByte> = new Vector<AGIByte>(MAX_RESOURCES);
 
 	/**
 		Defines an array of 256 boolean flags for use in the interpreter.
 		The first 16 flags are reserved by the interpreter. See specification for more information.
 		@see https://wiki.scummvm.org/index.php?title=AGI/Specifications/Internals#Flag
 	**/
-	public static var flags:Vector<Bool> = new Vector<Bool>(MAX_RESOURCES);
+	public static var FLAGS:Vector<Bool> = new Vector<Bool>(MAX_RESOURCES);
 
 	/**
 		Defines an array of 24 40 character wide strings for use in the interpreter.
 
-		Note that while some versions of the AGI Interpreter only supported 12 strings, some had the capacity to store 24. 
+		Note that while some versions of the AGI Interpreter only supported 12 strings, some had the capacity to store 24.
 		It is unknown if all 24 strings were used in those versions.
 
 		See specification for more information.
 		@see https://wiki.scummvm.org/index.php?title=AGI/Specifications/Internals#String
 	**/
-	public static var strings:Vector<String> = new Vector<String>(MAX_STRINGS);
+	public static var STRINGS:Vector<String> = new Vector<String>(MAX_STRINGS);
 
 	public function new() {}
 }
