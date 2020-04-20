48839f36288002b668d8e841ce305bbf3f85f3ef
Sean Galland <s_galland@msn.com>
2020-04-20 15:25:58 -0600

    Updated flag comments.

--- src/sage/agi/logic/commands/Flag.hx
+++ src/sage/agi/logic/commands/Flag.hx
@@ -15,28 +15,46 @@ class Flag {
 		AGIInterpreter.flags[n] = true;
 	}
 
+	/**
+		Set flag to true, where the flag is stored in a variable.
+		@param n Variable location to get the flag location.
+	**/
 	public static function setv(n:UInt) {
 		var flag = AGIInterpreter.variables[n];
 		AGIInterpreter.flags[flag] = true;
 	}
 
+	/**
+		Set flag to false.
+		@param n Flag location to set to false.
+	**/
 	public static function reset(n:UInt) {
 		AGIInterpreter.flags[n] = false;
 	}
 
+	/**
+		Set flag to false, where the flag is stored in a variable.
+		@param n Variable location to get the flag to set to false.
+	**/
 	public static function resetv(n:UInt) {
 		var flag = AGIInterpreter.variables[n];
 		AGIInterpreter.flags[flag] = false;
 	}
 
+	/**
+		Toggle flag value.
+		@param n Flag location to toggle.
+	**/
 	public static function toggle(n:UInt) {
 		AGIInterpreter.flags[n] = !AGIInterpreter.flags[n];
 	}
 
+	/**
+		Toggle flag value, where the flag is stored in a variable.
+		@param n Variable location to get the flag to set to toggle.
+	**/
 	public static function togglev(n:UInt) {
 		var flag = AGIInterpreter.variables[n];
 		AGIInterpreter.flags[flag] = !AGIInterpreter.flags[flag];
 	}
-
-	// TODO: Implement Flag Commands
 }
