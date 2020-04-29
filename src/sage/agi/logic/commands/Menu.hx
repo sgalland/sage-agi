3a1e82e5550ff15289698f38f8d34b2902956d3b
Sean Galland <s_galland@msn.com>
2020-04-28 20:49:01 -0600

    Added missing Command classes.

---
+++ src/sage/agi/logic/commands/Menu.hx
@@ -0,0 +1,9 @@
+package sage.agi.logic.commands;
+
+/**
+	Commands to work with managing menus.
+	@see https://wiki.scummvm.org/index.php?title=AGI/Specifications/Logic#Menu_management_commands
+**/
+class String {
+	// TODO: Implement menu commands
+}
---
+++ src/sage/agi/logic/commands/Other.hx
@@ -0,0 +1,9 @@
+package sage.agi.logic.commands;
+
+/**
+	Commands that are not fully understood.
+	@see https://wiki.scummvm.org/index.php?title=AGI/Specifications/Logic#Other_commands_2
+**/
+class Other {
+	// TODO: Implement other commands
+}
