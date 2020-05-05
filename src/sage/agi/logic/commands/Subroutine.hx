ce91bdf004ebdc2cd7b49ea482c0aa32ebce80ad
Sean Galland <sgalland@timeshareware.com>
2020-05-05 15:44:43 -0600

    Worked on running actions in the logic processor.

--- src/sage/agi/logic/commands/Subroutine.hx
+++ src/sage/agi/logic/commands/Subroutine.hx
@@ -5,5 +5,13 @@ package sage.agi.logic.commands;
 	@see https://wiki.scummvm.org/index.php?title=AGI/Specifications/Logic#Subroutine_call_commands
 **/
 class Subroutine {
+	/**
+		Calls a Logic file for execution.
+		@param resourceID
+	**/
+	public static function call(resourceID:UInt) {
+		LogicProcessor.execute(resourceID);
+	}
+
 	// TODO: Implement Subroutine call commands
 }
